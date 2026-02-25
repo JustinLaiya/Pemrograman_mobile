710720f93de2cfb1c8d9135f9a3739ab28fed48e
JustinLaiya <laiyajustin@gmail.com>
2026-02-25 15:13:14 +0700

    Pertemuan 03

---
+++ Pertemuan 03(PercabanganDanPerulanganDart)/ControlFlow.dart
@@ -0,0 +1,20 @@
+void main() {
+  String test = "Test2";
+  if (test == "Test1") {
+    print("Test1");
+  } else if (test == "Test2") {
+    print("Test2");
+  } else {
+    print("Bukan Test1 dan Test2");
+  }
+
+  if(test == "Test2") print("Test2 again");
+}
+
+void main2() {
+  
+  String test = "true";
+  if (test == "true") {
+    print("kebenaran");
+  }
+}
